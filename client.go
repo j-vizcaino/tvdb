89151d9324d6e67e2ddc146793d7e1ac00ae3b52
Jerome Vizcaino <jerome.vizcaino@datadoghq.com>
2018-08-05 17:04:03 +0200

    Add client documentation

--- client.go
+++ client.go
@@ -1,3 +1,4 @@
+// tvdb package provides a Go client implementation of The TVDB API v2
 package tvdb
 
 import (
@@ -14,11 +15,12 @@ var baseURL = url.URL{
 	Host:   "api.thetvdb.com",
 }
 
-type RequestOption func(*http.Request)
+type requestOption func(*http.Request)
 
+// QueryOption provides a query option function.
 type QueryOption func(*url.Values)
 
-func withLanguage(language string) RequestOption {
+func withLanguage(language string) requestOption {
 	return func (req *http.Request) {
 		if len(language) > 0 {
 			req.Header.Set("Accept-Language", language)
@@ -26,22 +28,32 @@ func withLanguage(language string) RequestOption {
 	}
 }
 
+// WithAiredSeasonNumber filters episodes matching the given season number.
+// This function should be used with EpisodesBySeriesID
 func WithAiredSeasonNumber(seasonNumber int) QueryOption {
 	return withQueryIntOption("airedSeason", seasonNumber)
 }
 
+// WithAiredEpisodeNumber filters episodes matching the given episode number.
+// This function should be used with EpisodesBySeriesID
 func WithAiredEpisodeNumber(episodeNumber int) QueryOption {
 	return withQueryIntOption("airedEpisode", episodeNumber)
 }
 
+// WithDVDSeasonNumber filters episodes matching the given DVD season number.
+// This function should be used with EpisodesBySeriesID
 func WithDVDSeasonNumber(seasonNumber int) QueryOption {
 	return withQueryIntOption("dvdSeason", seasonNumber)
 }
 
+// WithDVDEpisodeNumber filters episodes matching the given DVD episode number.
+// This function should be used with EpisodesBySeriesID
 func WithDVDEpisodeNumber(episodeNumber int) QueryOption {
 	return withQueryIntOption("dvdEpisode", episodeNumber)
 }
 
+// WithAbsoluteEpisodeNumber filters episodes matching the given absolute episode number.
+// This function should be used with EpisodesBySeriesID
 func WithAbsoluteEpisodeNumber(episodeNumber int) QueryOption {
 	return withQueryIntOption("absoluteNumber", episodeNumber)
 }
@@ -58,6 +70,10 @@ func withQueryOption(name, value string) QueryOption {
 	}
 }
 
+// ClientOptions represents options for a TVDB client.
+// Either APIKey or UserKey and Username are mandatory for login.
+// Language provides a hint to return result in the given language and can be changed dynamically
+// using the WithLanguage() helper.
 type ClientOptions struct {
 	APIKey   string
 	UserKey  string
@@ -72,6 +88,8 @@ type client struct {
 	options     ClientOptions
 }
 
+// NewClient creates a new TVDB client.
+// The function immediately tries to login and returns the handle of the new client.
 func NewClient(options ClientOptions) (*client, error) {
 	c := &client{
 		httpClient: &http.Client{},
@@ -84,6 +102,7 @@ func NewClient(options ClientOptions) (*client, error) {
 	return c, nil
 }
 
+// URL builds a request URL for the given path and options.
 func (c *client) URL(path string, options ...QueryOption) url.URL {
 	ret := baseURL
 	ret.Path = path
@@ -98,6 +117,7 @@ func (c *client) URL(path string, options ...QueryOption) url.URL {
 	return ret
 }
 
+// Languages returns a list of languages supported by The TVDB.
 func (c *client) Languages() ([]Language, error) {
 	var data LanguageData
 	fullURL := c.URL("/languages")
@@ -111,19 +131,23 @@ func (c *client) Languages() ([]Language, error) {
 	return data.Data, nil
 }
 
+// Token returns the JWT used for authentication.
 func (c *client) Token() string {
 	return c.token
 }
 
+// Options returns a copy of the options used by the client.
 func (c *client) Options() ClientOptions {
 	return c.options
 }
 
+// WithLanguage updates the client default language and returns a shallow copy of the client handle.
 func (c* client) WithLanguage(language string) *client {
 	c.options.Language = language
 	return c
 }
 
+// SieriesByID returns a single  series information given a TVDB ID.
 func (c *client) SeriesByID(id int) (*Series, error) {
 	var data SeriesData
 	fullURL := c.URL(fmt.Sprintf("/series/%d", id))
@@ -133,6 +157,10 @@ func (c *client) SeriesByID(id int) (*Series, error) {
 	return data.Data, nil
 }
 
+// EpisodesBySeriesID returns a list of episodes for the given series ID, optionally filtered by filters.
+//
+// When no filters are provided, this function returns all the episodes of the series.
+// Filters can be used to get, for example, all the episodes from a given season, or a single episode in a season.
 func (c *client) EpisodesBySeriesID(seriesID int,filters ...QueryOption) ([]Episode, error) {
 	uri := fmt.Sprintf("/series/%d/episodes", seriesID)
 	if len(filters) > 0 {
@@ -156,6 +184,9 @@ func (c *client) EpisodesBySeriesID(seriesID int,filters ...QueryOption) ([]Epis
 	return episodes, nil
 }
 
+// SearchSeriesByName returns a list of series matching name.
+//
+// Results are returned in the configured client language.
 func (c *client) SearchSeriesByName(name string) ([]SeriesSearchResult, error) {
 	var result SeriesSearchResults
 	fullURL := c.URL("/search/series", withQueryOption("name", name))
@@ -193,7 +224,7 @@ func (c *client) login() error {
 	return nil
 }
 
-func (c *client) get(url url.URL, out interface{}, options ...RequestOption) error {
+func (c *client) get(url url.URL, out interface{}, options ...requestOption) error {
 	req, err := http.NewRequest("GET", url.String(), nil)
 	if err != nil {
 		return err
